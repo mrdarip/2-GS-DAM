c74e0c491cdcd4113b2b4ee79da118d391a52a6b
mrdarip <darioelas2005@gmail.com>
2024-10-08 09:41:01 +0200

    completed EJ1

--- DINT/T1E1/app/src/main/java/com/mrdarip/T1E1/App.java
+++ DINT/T1E1/app/src/main/java/com/mrdarip/T1E1/App.java
@@ -3,15 +3,13 @@ package com.mrdarip.T1E1; //Modificar al package correcto
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-/**
- * Plantilla JAVAFX Autor: Curso y año: Objetivo de esta clase:
- */
 public class App extends Application {
 
     public static void main(String[] args) {
@@ -28,6 +26,16 @@ public class App extends Application {
         TextField discountTF = new TextField();
         
         Button calcB = new Button("Calcular");
+        
+        calcB.setOnAction((event)->{
+            double price = Double.parseDouble(priceTF.getText());
+            double discount = Double.parseDouble(discountTF.getText());
+            
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Descuento aplicado. Precio inicial: "+ price);
+            alert.setContentText("Descuento: " + discount + "\nPrecio final: " + (price * (1-(discount/100))));
+            alert.showAndWait();
+        });
 
         VBox vbox = new VBox(10,priceLB,priceTF,discountLB,discountTF, calcB); //Layout padre (contenedor de nodos). 10 px separación
         vbox.setPadding(new Insets(20));//Borde interno 20px
