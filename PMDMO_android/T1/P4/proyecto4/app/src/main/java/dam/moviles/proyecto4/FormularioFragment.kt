f8a17ddd22b0390621ccd391b057c1826bc322f6
mrdarip <darioelas2005@gmail.com>
2024-11-08 10:51:19 +0100

    updated proyecto 4

--- PMDMO_android/T1/P4/proyecto4/app/src/main/java/dam/moviles/proyecto4/FormularioFragment.kt
+++ PMDMO_android/T1/P4/proyecto4/app/src/main/java/dam/moviles/proyecto4/FormularioFragment.kt
@@ -18,7 +18,7 @@ class FormularioFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         setupBinding(inflater, container)
         setupTaskBar()
 
